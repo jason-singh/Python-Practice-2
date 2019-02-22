a4f6e618dd4801bda560b9e5588c57b99065e37b
Jason Singh <jasonpoonia12@gmail.com>
2019-02-22 22:48:31 +1300

    Finished up the program and even made a main routine and looped it.

--- Python/speedingticket.py
+++ Python/speedingticket.py
@@ -2,8 +2,13 @@
 #Trying to make a efficient and small script that can be used to track illegial drivers.
 #Jason Singh, 21 February
 
+#Modules
+import time
+
+#Variables
+fine = 0
 wanted_first = ["James", "Helga", "Zachary"]
-wanted_last = ["Wilson", "Norman", "Conroy "]
+wanted_last = ["Wilson" , "Norman", "Conroy "]
 #Lists
 a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 b = [10, 11, 12, 13, 14]
@@ -14,40 +19,87 @@ f = [30, 31, 32, 33, 34]
 g = [35, 36, 37, 38, 39]
 h = [40, 41, 42, 43, 44]
 i = [45]
+finelist = []
+
+#Functions
+def addtolist(CarSpeed, SpeedLimit, fine):
+    CarOverLimit = CarSpeed - SpeedLimit
+    if CarOverLimit in a:
+        fine = 30
+        finelist.append(fine)
+        print("$30 FINE")
+    elif CarOverLimit in b:
+        fine = 80
+        finelist.append(fine)
+        print("$80 FINE")
+    elif CarOverLimit in c:
+        fine = 120
+        finelist.append(fine)
+        print("$120 FINE")
+    elif CarOverLimit in d:
+        fine = 170
+        finelist.append(fine)
+        print("$170 FINE")
+    elif CarOverLimit in e:
+        fine = 230
+        finelist.append(fine)
+        print("$230 FINE")
+    elif CarOverLimit in f:
+        fine = 300
+        finelist.append(fine)
+        print("$300 FINE")
+    elif CarOverLimit in g:
+        fine = 400
+        finelist.append(fine)
+        print("$400 FINE")
+    elif CarOverLimit in h:
+        fine = 510
+        finelist.append(fine)
+        print("$510 FINE")
+    elif CarOverLimit >= 45:
+        fine = 630
+        finelist.append(fine)
+        print("$630 FINE")
+    else:
+        print("NO FINE")
 
 def wanted(First_Name, Last_Name):
     if First_Name in wanted_first and Last_Name in wanted_last:
-        print("{} {} has an arrest warrant for his name".format(First_Name, Last_Name))
+        print("========================================")
+        print("WARNING! WARNING! WARNING!")
+        print("{} {} HAS A ARREST WARRANT".format(First_Name, Last_Name))
+        print("========================================")
     else:
-        print("{} {} has no arrset warrant for his name".format(First_Name, Last_Name))
-speederlist = []
+        print("{} {} IS CLEAR\n".format(First_Name, Last_Name))
 
-First_Name = input("What is the driver's First Name: \n")
-Last_Name =  input("What is the driver's Last Name: \n")
-SpeedLimit = int(input("What is the speed limit: \n"))
-CarSpeed = int(input("What was the drivers speed: \n"))
+def menu():
+    print("______________________\n")
+    print("SPEED FINE CALCULATOR")
+    print("______________________\n")
+    print("1 | CALCULATE FINES")
+    print("2 | TOTAL FINES")
+    print("3 | EXIT\n")
+    x = input("ENTER 1 | 2 | 3\n")
+    return x
 
-
-CarOverLimit = CarSpeed - SpeedLimit
-if CarOverLimit in a:
-    print("You have to pay $30")
-elif CarOverLimit in b:
-    print("You have to pay $80")
-elif CarOverLimit in c:
-    print("You have to pay $120")
-elif CarOverLimit in d:
-    print("You have to pay $170")
-elif CarOverLimit >= e:
-    print("You have to pay $230")
-elif CarOverLimit in f:
-    print("You have to pay $300")
-elif CarOverLimit in g:
-    print("You have to pay $400")
-elif CarOverLimit in h:
-    print("You have to pay $510")
-elif CarOverLimit >= i:
-    print("You have to pay $630")
-else:
-    print("You have no fine to pay. Drive Safe")
-
-wanted(First_Name, Last_Name)
+#Main routine that runs in loop
+while True:
+    task = menu()
+    if task == "1":
+            First_Name = input("DRIVER'S FIRST NAME: \n").upper()
+            Last_Name =  input("DRIVER'S LAST NAME: \n").upper()
+            SpeedLimit = int(input("ROAD SPEED LIMIT: \n"))
+            CarSpeed = int(input("DRIVER'S SPEED: \n"))
+            addtolist(CarSpeed, SpeedLimit, fine)
+            wanted(First_Name, Last_Name)
+    elif task == "2":
+            if sum(finelist) == 0:
+                print("NO RECORDS")
+            else:
+                print("TOTAL FINE: {}".format(sum(finelist)))
+                time.sleep(1)
+                print("TOTAL AMOUNT OF FINES: {}".format(len(finelist)))
+    elif task == "3":
+        exit()
+    else:
+        print("INPUT VALUE NOT ALLOWED")
